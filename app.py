3cf09bb2bb875622a96f88fd6adcc31d3fe82d2e
novacraftsgm-jpg <novacrafts.gm@gmail.com>
2025-11-30 13:24:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3450,6 +3450,7 @@ def servis_user_detail(id):
     conn.close()
     return jsonify({'success': True})
 
-if __name__ == '__main__':
-    init_db()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
